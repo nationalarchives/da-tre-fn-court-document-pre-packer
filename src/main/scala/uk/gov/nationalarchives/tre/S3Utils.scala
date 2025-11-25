fddc2f679e8bfd5a3508eec52f9a7af32973f6bf
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-11-25 13:48:06 +0000

    add pagination getFileNames

--- src/main/scala/uk/gov/nationalarchives/tre/S3Utils.scala
+++ src/main/scala/uk/gov/nationalarchives/tre/S3Utils.scala
@@ -2,23 +2,38 @@ package uk.gov.nationalarchives.tre
 
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.{CopyObjectRequest, CopyObjectResponse, GetObjectRequest, ListObjectsV2Request, ListObjectsV2Response, PutObjectRequest}
+import software.amazon.awssdk.services.s3.model._
 
 import java.io.{BufferedReader, InputStreamReader}
+import scala.annotation.tailrec
 import scala.jdk.CollectionConverters._
 
 class S3Utils(s3Client: S3Client) {
 
   def getFileNames(bucketName: String, directory: String): Seq[String] = {
-    val listObjectsRequest = ListObjectsV2Request.builder()
-      .bucket(bucketName)
-      .prefix(directory)
-      .build()
+    val prefix = if (directory.endsWith("/")) directory else directory + "/"
 
-    val listObjectsResponse: ListObjectsV2Response = s3Client.listObjectsV2(listObjectsRequest)
+    @tailrec
+    def loop(token: Option[String], acc: Vector[String]): Vector[String] = {
+      val builder = ListObjectsV2Request.builder()
+        .bucket(bucketName)
+        .prefix(prefix)
+        .maxKeys(1000)
+      token.foreach(builder.continuationToken)
+      val resp = s3Client.listObjectsV2(builder.build())
 
-    listObjectsResponse.contents().asScala.map(_.key()).toSeq
-      .map(_.replace(s"$directory/", ""))
+      val pageKeys = resp.contents().asScala.iterator
+        .map(_.key())
+        .map(_.substring(prefix.length))
+        .filter(_.nonEmpty)
+        .toVector
+
+      val nextToken = Option(resp.nextContinuationToken())
+      val updated = acc ++ pageKeys // preserve ordering
+      if (resp.isTruncated) loop(nextToken, updated) else updated
+    }
+
+    loop(None, Vector.empty)
   }
 
   def getFileContent(bucketName: String, fileKey: String): String = {
--- src/test/scala/uk/gov/nationalarchives/tre/S3UtilsSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tre/S3UtilsSpec.scala
@@ -14,7 +14,8 @@ class S3UtilsSpec extends AnyFlatSpec with MockitoSugar {
 
     val expectedRequest = ListObjectsV2Request.builder()
       .bucket("test-tre-common-data")
-      .prefix("TRE-TEST/execution-id")
+      .prefix("TRE-TEST/execution-id/")
+      .maxKeys(1000)
       .build()
 
     val objectListing = ListObjectsV2Response.builder()
@@ -24,6 +25,7 @@ class S3UtilsSpec extends AnyFlatSpec with MockitoSugar {
         S3Object.builder().key("TRE-TEST/execution-id/parser.log").build(),
         S3Object.builder().key("TRE-TEST/execution-id/metadata.json").build()
       )
+      .isTruncated(false)
       .build()
 
     when(s3Client.listObjectsV2(expectedRequest)).thenReturn(objectListing)
@@ -34,5 +36,50 @@ class S3UtilsSpec extends AnyFlatSpec with MockitoSugar {
     val actualFileNames = s3Utils.getFileNames("test-tre-common-data", "TRE-TEST/execution-id")
 
     expectedFileNames shouldBe actualFileNames
+  }
+
+  "getFileNames" should "paginate across multiple pages" in {
+    val s3Client = mock[S3Client]
+    val bucket = "test-tre-common-data"
+    val prefixNoSlash = "TRE-TEST/execution-id"
+    val prefix = prefixNoSlash + "/"
+
+    val firstRequest = ListObjectsV2Request.builder()
+      .bucket(bucket)
+      .prefix(prefix)
+      .maxKeys(1000)
+      .build()
+
+    val secondRequest = ListObjectsV2Request.builder()
+      .bucket(bucket)
+      .prefix(prefix)
+      .maxKeys(1000)
+      .continuationToken("token-1")
+      .build()
+
+    val firstResponse = ListObjectsV2Response.builder()
+      .contents(
+        S3Object.builder().key(prefix + "file1.txt").build(),
+        S3Object.builder().key(prefix + "file2.txt").build()
+      )
+      .isTruncated(true)
+      .nextContinuationToken("token-1")
+      .build()
+
+    val secondResponse = ListObjectsV2Response.builder()
+      .contents(
+        S3Object.builder().key(prefix + "file3.txt").build(),
+        S3Object.builder().key(prefix + "file4.txt").build()
+      )
+      .isTruncated(false)
+      .build()
+
+    when(s3Client.listObjectsV2(firstRequest)).thenReturn(firstResponse)
+    when(s3Client.listObjectsV2(secondRequest)).thenReturn(secondResponse)
+
+    val s3Utils = new S3Utils(s3Client)
+    val files = s3Utils.getFileNames(bucket, prefixNoSlash)
+
+    files shouldBe Seq("file1.txt", "file2.txt", "file3.txt", "file4.txt")
   }
 }
